d0d2dbaa21595770871b3ebfa3dd49670730568a
MateusAlo <mateusalonso@usp.br>
2025-01-06 11:23:30 -0300

    fixing navsat transform not working correctly, ekf node and launch file working

--- src/ekf_localization/setup.py
+++ src/ekf_localization/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/ekf_localization_launch.py']),
+        ('share/' + package_name + '/config', ['config/navsat_transform.yaml', 'config/ekf_params.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ekf_node = ekf_localization.ekf_node:main',
         ],
     },
 )
